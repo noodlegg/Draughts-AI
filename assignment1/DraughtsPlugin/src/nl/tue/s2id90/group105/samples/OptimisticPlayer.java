8585000986fb50fcd5c3bd34313a396aaa34515e
NoodleGG <TGian98@gmail.com>
2019-02-22 21:14:10 +0100

    Renamed files
    groupNN changed to group105

--- assignment1/DraughtsPlugin/src/nl/tue/s2id90/group105/samples/OptimisticPlayer.java
+++ assignment1/DraughtsPlugin/src/nl/tue/s2id90/group105/samples/OptimisticPlayer.java
@@ -1,27 +1,25 @@
-package nl.tue.s2id90.groupNN.samples;
+package nl.tue.s2id90.group105.samples;
 
-import java.util.Collections;
+
 import java.util.List;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import org10x10.dam.game.Move;
 
 /**
- * A simple draughts player that plays random moves
+ * A simple draughts player that plays the first moves that comes to mind
  * and values all moves with value 0.
  * @author huub
  */
-public class UninformedPlayer extends DraughtsPlayer {
+public class OptimisticPlayer extends DraughtsPlayer {
 
-    public UninformedPlayer() {
-        super(UninformedPlayer.class.getResource("smiley.png"));
+    public OptimisticPlayer() {
+        super(UninformedPlayer.class.getResource("optimist.png"));
     }
-    
     @Override
     /** @return a random move **/
     public Move getMove(DraughtsState s) {
         List<Move> moves = s.getMoves();
-        Collections.shuffle(moves);
         return moves.get(0);
     }
 
--- assignment1/DraughtsPlugin/src/nl/tue/s2id90/group105/samples/UninformedPlayer.java
+++ assignment1/DraughtsPlugin/src/nl/tue/s2id90/group105/samples/UninformedPlayer.java
@@ -1,25 +1,27 @@
-package nl.tue.s2id90.groupNN.samples;
+package nl.tue.s2id90.group105.samples;
 
-
+import java.util.Collections;
 import java.util.List;
 import nl.tue.s2id90.draughts.DraughtsState;
 import nl.tue.s2id90.draughts.player.DraughtsPlayer;
 import org10x10.dam.game.Move;
 
 /**
- * A simple draughts player that plays the first moves that comes to mind
+ * A simple draughts player that plays random moves
  * and values all moves with value 0.
  * @author huub
  */
-public class OptimisticPlayer extends DraughtsPlayer {
+public class UninformedPlayer extends DraughtsPlayer {
 
-    public OptimisticPlayer() {
-        super(UninformedPlayer.class.getResource("optimist.png"));
+    public UninformedPlayer() {
+        super(UninformedPlayer.class.getResource("smiley.png"));
     }
+    
     @Override
     /** @return a random move **/
     public Move getMove(DraughtsState s) {
         List<Move> moves = s.getMoves();
+        Collections.shuffle(moves);
         return moves.get(0);
     }
 
