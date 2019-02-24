75f079fae4efae835da818734402cef021d8ed50
NoodleGG <TGian98@gmail.com>
2019-02-24 20:58:55 +0100

    PlayerTest added
    has the most heuristics, yet loses against Player55 and V1

---
+++ assignment1/DraughtsPlugin/src/nl/tue/s2id90/group105/MyDraughtsPlayerTest.java
@@ -0,0 +1,370 @@
+package nl.tue.s2id90.group105;
+
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+import java.util.Collections;
+import java.util.List;
+import nl.tue.s2id90.draughts.DraughtsState;
+import nl.tue.s2id90.draughts.player.DraughtsPlayer;
+import org10x10.dam.game.Move;
+
+/**
+ * Implementation of the DraughtsPlayer interface.
+ *
+ * @author huub
+ */
+// ToDo: rename this class (and hence this file) to have a distinct name
+//       for your player during the tournament
+public class MyDraughtsPlayerTest extends DraughtsPlayer {
+
+    private int bestValue = 0;
+    int maxSearchDepth;
+
+    /**
+     * boolean that indicates that the GUI asked the player to stop thinking.
+     */
+    private boolean stopped;
+
+    public MyDraughtsPlayerTest(int maxSearchDepth) {
+        super("best.png"); // ToDo: replace with your own icon
+        this.maxSearchDepth = maxSearchDepth;
+    }
+
+    @Override
+    public Move getMove(DraughtsState s) {
+        Move bestMove = null;
+        bestValue = 0;
+        DraughtsNode node = new DraughtsNode(s);    // the root of the search tree
+        try {
+            // compute bestMove and bestValue in a call to alphabeta
+            bestValue = alphaBeta(node, MIN_VALUE, MAX_VALUE, maxSearchDepth, maxSearchDepth);
+
+            // store the bestMove found uptill now
+            // NB this is not done in case of an AIStoppedException in alphaBeat()
+            bestMove = node.getBestMove();
+
+            // print the results for debugging reasons
+            System.err.format(
+                    "%s: depth= %2d, best move = %5s, value=%d\n",
+                    this.getClass().getSimpleName(), maxSearchDepth, bestMove, bestValue
+            );
+        } catch (AIStoppedException ex) {  /* nothing to do */        }
+
+        if (bestMove == null) {
+            System.err.println("no valid move found!");
+            return getRandomValidMove(s);
+        } else {
+            return bestMove;
+        }
+    }
+
+    /**
+     * This method's return value is displayed in the AICompetition GUI.
+     *
+     * @return the value for the draughts state s as it is computed in a call to
+     * getMove(s).
+     */
+    @Override
+    public Integer getValue() {
+        return bestValue;
+    }
+
+    /**
+     * Tries to make alphabeta search stop. Search should be implemented such
+     * that it throws an AIStoppedException when boolean stopped is set to true;
+     *
+     */
+    @Override
+    public void stop() {
+        stopped = true;
+    }
+
+    /**
+     * returns random valid move in state s, or null if no moves exist.
+     */
+    Move getRandomValidMove(DraughtsState s) {
+        List<Move> moves = s.getMoves();
+        Collections.shuffle(moves);
+        return moves.isEmpty() ? null : moves.get(0);
+    }
+
+    /**
+     * Implementation of alphabeta that automatically chooses the white player
+     * as maximizing player and the black player as minimizing player.
+     *
+     * @param node contains DraughtsState and has field to which the best move
+     * can be assigned.
+     * @param alpha
+     * @param beta
+     * @param depth maximum recursion Depth
+     * @return the computed value of this node
+     * @throws AIStoppedException
+     *
+     */
+    int alphaBeta(DraughtsNode node, int alpha, int beta, int currentDepth, int maxDepth)
+            throws AIStoppedException {
+        if (node.getState().isWhiteToMove()) {
+            return alphaBetaMax(node, alpha, beta, currentDepth, maxDepth);
+        } else {
+            return alphaBetaMin(node, alpha, beta, currentDepth, maxDepth);
+        }
+    }
+
+    /**
+     * Does an alphabeta computation with the given alpha and beta where the
+     * player that is to move in node is the minimizing player.
+     *
+     * <p>
+     * Typical pieces of code used in this method are:
+     * <ul> <li><code>DraughtsState state = node.getState()</code>.</li>
+     * <li><code> state.doMove(move); .... ; state.undoMove(move);</code></li>
+     * <li><code>node.setBestMove(bestMove);</code></li>
+     * <li><code>if(stopped) { stopped=false; throw new AIStoppedException(); }</code></li>
+     * </ul>
+     * </p>
+     *
+     * @param node contains DraughtsState and has field to which the best move
+     * can be assigned.
+     * @param alpha
+     * @param beta
+     * @param depth maximum recursion Depth
+     * @return the compute value of this node
+     * @throws AIStoppedException thrown whenever the boolean stopped has been
+     * set to true.
+     */
+    int alphaBetaMin(DraughtsNode node, int alpha, int beta, int currentDepth, int maxDepth)
+            throws AIStoppedException {
+        if (stopped) {
+            stopped = false;
+            throw new AIStoppedException();
+        }
+        DraughtsState state = node.getState();
+        // ToDo: write an alphabeta search to compute bestMove and value
+        Move bestMove = null;
+        // Evaluate all endStates or when depth is 0
+        if (state.isEndState() || currentDepth == 0) {
+            return evaluate(state);
+        } else {
+            // List all possible moves
+            List<Move> moves = state.getMoves();
+            int currentValue = Integer.MAX_VALUE;
+
+            for (Move move : moves) {
+                // Do a move to reach next state
+                state.doMove(move);
+                // Get nextValue with recursive call
+                int nextValue = alphaBetaMax(node, alpha, beta, currentDepth - 1, maxDepth);
+                // If next value is smaller than the current
+                if (nextValue < currentValue) {
+                    bestMove = move;
+                    currentValue = nextValue;
+                }
+                beta = Math.min(beta, currentValue);
+                state.undoMove(move);
+                // Alpha cutoff
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            if (currentDepth == maxDepth) {
+                node.setBestMove(bestMove);
+            }
+            return currentValue;
+        }
+    }
+
+    int alphaBetaMax(DraughtsNode node, int alpha, int beta, int currentDepth, int maxDepth)
+            throws AIStoppedException {
+        if (stopped) {
+            stopped = false;
+            throw new AIStoppedException();
+        }
+        DraughtsState state = node.getState();
+        // ToDo: write an alphabeta search to compute bestMove and value
+        Move bestMove = null;
+        // Evaluate all endStates or when currentDepth is 0
+        if (state.isEndState() || currentDepth == 0) {
+            return evaluate(state);
+        } else {
+            // List all possible moves
+            List<Move> moves = state.getMoves();
+            int currentValue = Integer.MIN_VALUE;
+
+            for (Move move : moves) {
+                // Do a move to reach next state
+                state.doMove(move);
+                // Get nextValue with recursive call
+                int nextValue = alphaBetaMin(node, alpha, beta, currentDepth - 1, maxDepth);
+                // If next value is larger than the current
+                if (nextValue > currentValue) {
+                    bestMove = move;
+                    currentValue = nextValue;
+                }
+                alpha = Math.max(alpha, currentValue);
+                state.undoMove(move);
+                // Beta cutoff
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            if (currentDepth == maxDepth) {
+                node.setBestMove(bestMove);
+            }
+            return currentValue;
+        }
+    }
+
+    /**
+     * A method that evaluates the given state.
+     */
+    // ToDo: write an appropriate evaluation function
+    int evaluate(DraughtsState state) {
+        // Check if someone won
+        if (state.isEndState()) {
+            if (state.isWhiteToMove()) {
+                // If black won
+                return Integer.MIN_VALUE + 1;
+            } else { // If white won
+                return Integer.MAX_VALUE - 1;
+            }
+        }
+        int result = 0;
+        result += evalPieceCount(state) * 10;
+        result += evalAttackCount(state);
+        result += evalGoodPosition(state);
+        result += evalTempiCount(state);
+        return result;
+    }
+
+    private int evalPieceCount(DraughtsState state) {
+        int[] pieces = state.getPieces();
+        int totalScore = 0;
+        for (int i = 0; i < pieces.length; i++) {
+            if (pieces[i] == 1) { // White pawn
+                totalScore += 1;
+            } else if (pieces[i] == 2) { // Black pawn
+                totalScore -= 1;
+            } else if (pieces[i] == 3) { // White king
+                totalScore += 3;
+            } else if (pieces[i] == 4) { // Black king
+                totalScore -= 3;
+            }
+        }
+        return totalScore;
+    }
+
+    private int evalAttackCount(DraughtsState state) {
+        int attackCount = 0;
+        List<Move> moves = state.getMoves();
+        if (state.isWhiteToMove()) { // White attack moves
+            for (Move move : moves) {
+                if (move.isCapture()) {
+                    attackCount++;
+                }
+            }
+        } else { // Black attack moves
+            for (Move move : moves) {
+                if (move.isCapture()) {
+                    attackCount--;
+                }
+            }
+        }
+        return attackCount;
+    }
+    
+    private int evalTempiCount(DraughtsState state) {
+        int totalScore = 0;
+        int[] pieces = state.getPieces();
+        for (int i = 0; i < pieces.length; i++) {
+            if (i < 6) { // Top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 9;
+                }
+            } else if (6 <= i && i < 11) { // 2nd top row
+                if (pieces[i] == 1 | pieces[i] == 3) { // If white
+                    totalScore += 8;
+                } else { // Else black
+                    totalScore -= 2;
+                }
+            } else if (16 <= i && i < 21) { // 3rd top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 7;
+                } else {
+                    totalScore -= 3;
+                }
+            } else if (21 <= i && i < 26) { // 4th top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 6;
+                } else {
+                    totalScore -= 4;
+                }
+            } else if (26 <= i && i < 31) { // 5th top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 5;
+                } else {
+                    totalScore -= 5;
+                }
+            } else if (31 <= i && i < 36) { // 6th top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 4;
+                } else {
+                    totalScore -= 6;
+                }
+            } else if (36 <= i && i < 41) { // 7th top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 3;
+                } else {
+                    totalScore -= 7;
+                }
+            } else if (41 <= i && i < 46) { // 8th top row
+                if (pieces[i] == 1 | pieces[i] == 3) {
+                    totalScore += 2;
+                } else {
+                    totalScore -= 8;
+                }
+            } else if (46 <= i && i < pieces.length) { // Bottom row
+                if (pieces[i] == 2 | pieces[i] == 4) { // Black
+                    totalScore -= 9;
+                }
+            }
+        }
+        return totalScore;
+    }
+    
+    private int evalGoodPosition(DraughtsState state) {
+        int totalScore = 0;
+        int pieces[] = state.getPieces();
+        for (int i = 0; i < pieces.length; i++) {
+            if ("middle".equals(getPosition(i))) { // If piece in middle
+                if (pieces[i] == 1 || pieces[i] == 3) { // If white
+                    totalScore++;
+                } else { // Else black
+                    totalScore--;
+                }
+            } else if ("top".equals(getPosition(i))) { // If piece at top
+                if (pieces[i] == 2 || pieces[i] == 4) { // Black at home
+                    totalScore--;
+                }
+            } else if ("bottom".equals(getPosition(i))) { // If piece at bottom
+                if (pieces[i] == 1 || pieces[i] == 3) { // White at home
+                    totalScore++;
+                }
+            }
+        }
+        return totalScore;
+    }
+    
+    private String getPosition(int i) {
+        if (i < 6) {
+            return "top";
+        } else if (i > 45) {
+            return "bottom";
+        } else if (i % 10 == 5) {
+            return "right";
+        } else if (i % 10 == 6) {
+            return "left";
+        } else {
+            return "middle";
+        }
+    }
+}
